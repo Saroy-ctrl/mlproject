15771235fc1caaa0229410a913bd8b087004bd5f
Saroy-ctrl <saanann12@gmail.com>
2025-12-31 12:56:59 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
        dir_path = os.path.dirname(file_path)
@@ -14,12 +15,17 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test, models):
+def evaluate_models(X_train,y_train,X_test,y_test, models,param):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+            
 
             # Predicting the test set results
             y_train_pred = model.predict(X_train)
